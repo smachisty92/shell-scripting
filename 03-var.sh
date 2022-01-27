a982f3aa2d32b551156c30e1eeb98aee56235ff1
csyed <smachisty92@gmail.com>
2022-01-27 23:15:25 +0530

    adding variable dynamic

--- 03-var.sh
+++ 03-var.sh
@@ -5,3 +5,8 @@ Student_name="syed"
 echo Student name is $Student_name
 
 echo student name is ${Student_name}
+
+# how to get the data dynamically
+
+Date=2022-01-21
+echo Good Morning, Today date is $Date
