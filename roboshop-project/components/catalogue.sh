c66c5f705df7efe3f3ec716dcce301030faeb0c9
csyed <smachisty92@gmail.com>
2022-02-06 22:28:01 +0530

    adding roboshop user and chown

--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -75,3 +75,5 @@ cp -r catalogue-main /home/roboshop/catalogue &>>$LOG_FILE
 echo "Install NodeJS Dependencies"
 cd /home/roboshop/catalogue
 npm install &>>$LOG_FILE
+
+chown roboshop:roboshop /home/roboshop/ -R
