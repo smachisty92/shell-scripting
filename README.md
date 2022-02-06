2fe9a6d6a0363675bad4f38e3fe8a64fdfb9007b
csyed <smachisty92@gmail.com>
2022-02-06 21:52:06 +0530

    moving the frontend code into nginx and starting the nginx

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # shell-scripting
+
+
+## Sed editor
+
+#Sed with -i edit the file
+#sed without -i will print the change on output
+# -e is for multiple conditions in sed command
+# -e cond1 -e cond2
+
+#'''bash
+#  search and replace /Subsctiute
+
+#sed -i -e 's/root/ROOT/g' -e 's/admin/ADMIN/g' sample.txt
+
+# DElete lines and Add lines
+
+#sed -i -e '1d' -e '/root/ d' sample.txt
+
+#sed -i -e '/root/ 1 Hello world' sample.txt
+#sed -i -e '1 a Hello World' sample.txt
+#sed -i -e '1 c Hellow world' sample.txt
+#'''
--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,44 +1,7 @@
 #The frontend is the service in RobotShop to serve the web content over Nginx.
-#
-#To Install Nginx.
-#
-#```
-## yum install nginx -y
-## systemctl enable nginx
-## systemctl start nginx
-#
-#```
-#
-#Let's download the HTDOCS content and deploy under the Nginx path.
-#
-#```
-## curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-#
-#```
-#
-#Deploy in Nginx Default Location.
-#
-#```
-## cd /usr/share/nginx/html
-## rm -rf *
-## unzip /tmp/frontend.zip
-## mv frontend-main/* .
-## mv static/* .
-## rm -rf frontend-master README.md
-## mv localhost.conf /etc/nginx/default.d/roboshop.conf
-#
-#```
-#
-#Finally restart the service once to effect the changes.
-#
-#```
-## systemctl restart nginx
-#
-#```
 
 # we are removing the log file of every action
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
+source components/common.sh
 
 
 echo "InstallingNGINX"
