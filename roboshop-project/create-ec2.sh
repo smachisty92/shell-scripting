4ae40a41d605d3087194a49fe120e46d80406d3e
csyed <smachisty92@gmail.com>
2022-02-10 14:51:27 +0530

    ec2 run instance with tags

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -9,8 +9,6 @@ if [ -z "${INSTANCE_NAME}" ]; then
   exit
 fi
 
-
-
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if [ -z "${AMI_ID}" ]; then
@@ -24,13 +22,14 @@ PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANC
 
 
 # if we want to run a instance and create if its not there with spot isntance
+
 if [ -z "${PRIVATE_IP}" ]; then
   SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allports-open --query "SecurityGroups[*].GroupId" --output text)
   if [ -z "${SG_ID}" ]; then
     echo -e "Security group allports-open does not exist"
     exit
   fi
-  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_ID}}]" "ResourceType=spot-instances-request, Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}" &>>$LOG
+  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request, Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}" &>>$LOG
   echo -e "\e[1m Instance Created\e[0m"
 else
   echo "Instance ${INSTANCE_NAME} is already exits, Henace not creating"
