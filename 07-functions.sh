0b123db95a46b38c53d8d6f9370d753a2302d021
csyed <smachisty92@gmail.com>
2022-02-10 18:35:26 +0530

    root user updates

---
+++ 07-functions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# a set of data to a name is called as variable
+# a set to commands to a name is called as function
+
+#Declare a function
+
+SAMPLE() {
+  echo "Welcome to DevOps Training"
+}
+
+# Call the function
+
+SAMPLE
