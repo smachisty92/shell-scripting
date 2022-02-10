19eb328fcd8a299106e702cedd2501dbe6cb5581
csyed <smachisty92@gmail.com>
2022-02-10 11:47:18 +0530

    ec2 run instance

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if [ -z "${AMI_ID}" ]; then
   echo "Unanble to find Image AMI_ID"
 else
   echo AMI ID = ${AMI_ID}
 fi
+
+# if we want to run a instance
+
+aws ec2 run-instance --image-id ${AMI_ID} --instance-type t3.micro --output text
+
