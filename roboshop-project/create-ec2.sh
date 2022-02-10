b74550ce4df2ac89ac6a180688417bcf50a8e2f1
csyed <smachisty92@gmail.com>
2022-02-10 12:01:29 +0530

    ec2 run instance with tags

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -12,11 +12,12 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 
 if [ -z "${AMI_ID}" ]; then
   echo "Unanble to find Image AMI_ID"
+  exit
 else
   echo AMI ID = ${AMI_ID}
 fi
 
 # if we want to run a instance
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=${INSTANCE_ID}}]"
 
