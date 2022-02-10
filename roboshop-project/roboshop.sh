3adc7fda3c83632149a71b521fa37ef03591baac
csyed <smachisty92@gmail.com>
2022-02-10 17:55:23 +0530

    root user updates

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo -e "\e[1;31m you should be root user to execute this script...\e[0m"
+  exit
+fi
+
+
 if [ -f components/$1.sh ]; then
   bash components/$1.sh
 else
