18ce252f778f8747c7d488a9eed3776792e2ba73
csyed <smachisty92@gmail.com>
2022-02-10 21:55:42 +0530

    root user updates

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -1,7 +1,6 @@
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
-
 STAT() {
   if [ $1 -eq 0 ]; then
     echo -e "\e[1;32m SUCCESS\e[0m"
@@ -9,4 +8,60 @@ STAT() {
     echo -e "\e[1;31m FAILED\e[0m"
     exit 2
   fi
+}
+
+NODEJS() {
+  COMPONENT=$1
+  source components/common.sh
+
+  echo "NodeJS repo"
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>$LOG_FILE
+  STAT $?
+
+  echo " Install NodeJS"
+  yum install nodejs gcc-c++ -y &>>$LOG_FILE
+  STAT $?
+
+  echo "Create App User"
+  useradd roboshop &>>$LOG_FILE
+  STAT $?
+
+  echo "Download $(COMPONENT) code"
+  curl -s -L -o /tmp/$(COMPONENT).zip "https://github.com/roboshop-devops-project/$(COMPONENT)/archive/main.zip" &>>$LOG_FILE
+  STAT $?
+
+  echo "Extract $(COMPONENT) code"
+  cd /tmp/
+  unzip -o $(COMPONENT).zip &>>$LOG_FILE
+  STAT $?
+
+  echo "clear old catalogue"
+  rm -rf /home/roboshop/$(COMPONENT)
+  STAT $?
+
+  echo "moving $(COMPONENT) content"
+  cp -r user-main /home/roboshop/$(COMPONENT) &>>$LOG_FILE
+  STAT $?
+
+  echo "Install NodeJS Dependencies"
+  cd /home/roboshop/$(COMPONENT)
+  npm install &>>$LOG_FILE
+  STAT $?
+
+  chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
+  STAT $?
+
+  echo "update systemd file"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/$(COMPONENT)/systemd.service &>>$LOG_FILE
+  STAT $?
+
+  echo "SetUp user systemd file"
+  mv /home/roboshop/$(COMPONENT)/systemd.service /etc/systemd/system/$(COMPONENT).service &>>$LOG_FILE
+  STAT $?
+
+  echo "Start $(COMPONENT)"
+  systemctl daemon-reload &>>$LOG_FILE
+  systemctl enable $(COMPONENT) &>>$LOG_FILE
+  systemctl start $(COMPONENT) &>>$LOG_FILE
+  STAT $?
 }
--- roboshop-project/components/user.sh
+++ roboshop-project/components/user.sh
@@ -1,1 +1,7 @@
-echo Installing user
+#This service is responsible for showing the list of items that are to be sold by the RobotShop e-commerce portal.
+
+
+
+source components/common.sh
+
+NODEJS user
