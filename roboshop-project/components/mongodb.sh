bb4adfacc8e2310900e3878a8036800fac884b89
csyed <smachisty92@gmail.com>
2022-02-11 18:13:59 +0530

    root user updates

---
+++ roboshop-project/components/mongodb.sh
@@ -0,0 +1,33 @@
+source components/common.sh
+
+echo "Download MongoDb repo file"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+STAT $?
+
+echo "Install MongoDB"
+yum install -y mongodb-org &>>$LOG_FILE
+STAT $?
+
+echo "Update MongoDB Config file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  &>>$LOG_FILE
+STAT $?
+
+echo "Start Database"
+systemctl enable mongod  &>>$LOG_FILE && systemctl start mongod &>>$LOG_FILE
+STAT $?
+
+echo "Download Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"  &>>$LOG_FILE
+STAT $?
+
+echo "Extract Schema"
+cd /tmp/
+unzip -o mongodb.zip  &>>$LOG_FILE
+STAT $?
+
+echo "Load Schema"
+cd mongodb-main
+for app in catalogue users ; do
+  mongo < ${app}.js &>>$LOG_FILE
+done
+STAT $?
